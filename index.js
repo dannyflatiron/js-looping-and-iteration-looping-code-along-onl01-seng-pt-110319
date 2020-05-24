9f05d1ed2f7bc20276d726d63bd1fe6486761959
dannyflatiron <dannyflatiron@gmail.com>
2020-05-24 00:32:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let messages = []
+    let i = 0;
+    while (i < names.length){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        i++;
+    }
+    return messages
+}
+
+function countDown(number){
+    while (number > 0){
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
